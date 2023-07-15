92821537c452e3620002e66891795b933d43bff4
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2023-07-15 20:43:24 +0400

    change

--- src/Components/About/JobExp/Diesel/DieselVideo.jsx
+++ src/Components/About/JobExp/Diesel/DieselVideo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import video from "../../../../Assets/dieselclip.mp4";
+import video from "../../../../Assets/dieselClip.mp4";
 
 function DieselVideo() {
   return (
